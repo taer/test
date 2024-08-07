a7ff5b7eff62c60a62b7506954c338133d48b15d
Robert Macaulay <robert.macaulay@hotelengine.com>
2024-08-06 22:03:49 -0500

    Repro

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,17 @@
 package org.example
 
 import kotlinx.coroutines.asContextElement
-import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.isPresent
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
-import kotlinx.coroutines.yield
-import java.lang.annotation.Inherited
-import java.util.concurrent.Executors
 import kotlin.concurrent.thread
 
 
-object Holder :ThreadLocal<Int>()
-suspend fun main()  {
+object Holder : ThreadLocal<Int>()
 
+suspend fun main() {
     Holder.set(11)
     val responses = Channel<Int>(1)
 
@@ -31,35 +25,60 @@ suspend fun main()  {
         println("Send context, HP:${Holder.get()} on ${Thread.currentThread().name}")
         responses.trySend(2)
     }
-    thread(start = true) {
-        Thread.sleep(3000)
-        println("Send context, HP:${Holder.get()} on ${Thread.currentThread().name}")
-        responses.trySend(3)
-    }
-    thread(start = true) {
-        Thread.sleep(4000)
-        println("Send context, HP:${Holder.get()} on ${Thread.currentThread().name}")
-        responses.trySend(4)
-    }
+//    thread(start = true) {
+//        Thread.sleep(3000)
+//        println("Send context, HP:${Holder.get()} on ${Thread.currentThread().name}")
+//        responses.trySend(3)
+//    }
+//    thread(start = true) {
+//        Thread.sleep(4000)
+//        println("Send context, HP:${Holder.get()} on ${Thread.currentThread().name}")
+//        responses.trySend(4)
+//    }
 
-    withContext( Holder.asContextElement()) {
-        println("In context, HP:${Holder.isPresent()} on ${Thread.currentThread().name}")
+//    fails(responses)
+//    fails2(responses)
+    failsToo(responses)
+
+}
+private suspend fun failsToo(responses: Channel<Int>) {
+    withContext(Holder.asContextElement()) {
+        println("In context, HP:${Holder.isPresent()}:${Holder.get()} on ${Thread.currentThread().name}")
+        for (i in responses) {
+            println("Receiving $i HP:${Holder.isPresent()}:${Holder.get()} on ${Thread.currentThread().name}")
+        }
+    }
+}
+
+private suspend fun fails(responses: Channel<Int>) {
+    withContext(Holder.asContextElement()) {
+        println("In context, HP:${Holder.isPresent()}:${Holder.get()} on ${Thread.currentThread().name}")
         val x = flow {
             coroutineScope {
-                println("hi")
                 for (i in responses) {
-                    println("Receiving $i HP:${Holder.isPresent()} on ${Thread.currentThread().name}")
+                    println("Receiving $i HP:${Holder.isPresent()}:${Holder.get()} on ${Thread.currentThread().name}")
                     emit(i)
-                    println("emit $i HP:${Holder.isPresent()} on ${Thread.currentThread().name}")
+                    println("emit $i HP:${Holder.isPresent()}:${Holder.get()} on ${Thread.currentThread().name}")
                 }
             }
         }
 
-        x.collect{
-            println("Flow Receiving $it HP:${Holder.isPresent()} on ${Thread.currentThread().name}")
+        x.collect {
+            println("Flow Receiving $it HP:${Holder.isPresent()}:${Holder.get()} on ${Thread.currentThread().name}")
         }
 
 
     }
-
 }
+
+private suspend fun fails2(responses: Channel<Int>) {
+    withContext(Holder.asContextElement()) {
+        println("In context, HP:${Holder.isPresent()}:${Holder.get()} on ${Thread.currentThread().name}")
+        coroutineScope {
+            for (i in responses) {
+                println("Receiving $i HP:${Holder.isPresent()}:${Holder.get()} on ${Thread.currentThread().name}")
+            }
+        }
+    }
+}
+
